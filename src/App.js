49e5bf2024cc38d5fdc5e7ddaab5f00ea79ef3ea
Josh Herrera <joshherrera09@gmail.com>
2020-11-19 13:01:56 -0800

    About.js in the works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import About from './components/About';
+import Landing from './components/Landing';
 
 function App() {
-  return <div>Hello World!</div>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/about">
+          <About></About>
+        </Route>
+        <Route path="/" exact>
+          <Landing></Landing>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
