3917e65166bf0cf34d5b6e22948f19f3c44c5616
Josh Herrera <joshherrera09@gmail.com>
2020-11-19 13:42:57 -0800

    Contact.js done

---
+++ src/components/Contact.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
+
+const Contact = ({ contactEmail, contactSection, socialLinks }) => {
+    return (
+        <div className="contact_container">
+            <h1>Get in Touch</h1>
+            <p>{contactSection}</p>
+            <br></br>
+            <a className="email_link" href={`mailto:${contactEmail}`}>
+                {contactEmail}
+            </a>
+            <div className="social_links">
+                <ul>
+                    <li>
+                        <a href={`mailto:${contactEmail}`}>
+                            <FaEnvelope></FaEnvelope>
+                        </a>
+                    </li>
+                    <li>
+                        <a href={socialLinks[0].url}>
+                            <FaLinkedin></FaLinkedin>                          
+                        </a>               
+                    </li>
+                    <li>
+                        <a href={socialLinks[1].url}>
+                            <FaGithub></FaGithub>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,11 @@ const Header = ({name, contactEmail}) => {
     return (
         <div>
             <div className="header_container">
+                <Link to="/"><h1>{name}</h1></Link>
                 <nav>
                     <ul>
                         <li><Link to="/about">About</Link></li>
-                        <li><a href={`mailto:${contactEmail}`} /></li>
+                        <li><a href={`mailto:${contactEmail}`}>Contact</a></li>
                     </ul>
                 </nav>
             </div>
