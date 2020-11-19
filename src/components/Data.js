40384740ebb585a770ed85bcd8b2cacd6cc369fc
Josh Herrera <joshherrera09@gmail.com>
2020-11-19 10:29:33 -0800

    my data entered into Data.js

---
+++ src/components/Data.js
@@ -0,0 +1,77 @@
+export default {
+    // 
+    name: 'Josh Herrera',
+
+    //Contact email
+    contactEmail: 'joshherrera09@gmail.com',
+
+    // Name for top of home page
+    homePageName: 'Josh Herrera',
+
+    // Home page about me
+    aboutMe: "Aspiring Full-Stack Web Developer, currently enrolled in the UCSD Web Development Coding Bootcamp",
+
+    homePageImage: "",
+
+    // Projects
+    projects: [
+        {
+            id: 1,
+            title: "",
+            thumbnail: "",
+            url: ""
+        },
+        {
+            id: 2,
+            title: "",
+            thumbnail: "",
+            url: ""
+        },
+        {
+            id: 3,
+            title: "",
+            thumbnail: "",
+            url: ""
+        },
+        {
+            id: 4,
+            title: "",
+            thumbnail: "",
+            url: ""
+        },
+        {
+            id: 5,
+            title: "",
+            thumbnail: "",
+            url: ""
+        },
+        {
+            id: 6,
+            title: "",
+            thumbnail: "",
+            url: ""
+        }
+    ],
+
+    // Short snippet of how to contact me
+    contactSection: "",
+
+    social: [
+        {
+            name: "LinkedIn",
+            url: "",
+        },
+        {
+            name: "GitHub",
+            url: "",
+        },
+        {
+            name: "Twitter",
+            url: "",
+        },
+        {
+            name: "Resume",
+            url: "",
+        }
+    ]
+}
