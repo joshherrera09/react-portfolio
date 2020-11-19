5c331bf191011157045381984fddc7eaf1614610
Josh Herrera <joshherrera09@gmail.com>
2020-11-19 11:49:09 -0800

    Data.js almost finished

--- src/components/Data.js
+++ src/components/Data.js
@@ -17,57 +17,69 @@ export default {
     projects: [
         {
             id: 1,
-            title: "",
+            title: "Password Generator",
+            text: "I created this applicatin to generate a random password based on the users criteria.",
             thumbnail: "",
-            url: ""
+            github: "https://github.com/joshherrera09/PW-generator",
+            deployed: "https://joshherrera09.github.io/PW-generator/"
         },
         {
             id: 2,
-            title: "",
+            title: "Weather Dashboard",
+            text: "I created the Weather Dashboard for theuser to be able to search any given city, and receive the current forecast as well as a 5-day forecast.",
             thumbnail: "",
-            url: ""
+            github: "https://github.com/joshherrera09/weather-dashboard",
+            deployed: "https://joshherrera09.github.io/weather-dashboard/"
         },
         {
             id: 3,
-            title: "",
+            title: "Quarantine Day Planner",
+            text: "The Quarantine Day Planner helps users plan their days during the COVID 19 pandemic.",
             thumbnail: "",
-            url: ""
+            github: "ttps://github.com/msurio/project1",
+            deployed: "https://msurio.github.io/project1/"
         },
         {
             id: 4,
-            title: "",
+            title: "Work Day Scheduler",
+            text: "The work Day Scheduler allows the user to plan their day during regular work hours.",
             thumbnail: "",
-            url: ""
+            github: "https://github.com/joshherrera09/work-day-scheduler",
+            deployed: "ttps://joshherrera09.github.io/work-day-scheduler/"
         },
         {
             id: 5,
-            title: "",
+            title: "Boo Chat",
+            text: "Hop in our virtual Halloween Caht Room!",
             thumbnail: "",
-            url: ""
+            github: "ttps://github.com/msurio/project2",
+            deployed: "https://project2-ucsd.herokuapp.com/"
         },
         {
             id: 6,
-            title: "",
+            title: "Fitness Tracker",
+            text: "The Fitness Tracker is a great way to log your workouts and progress",
             thumbnail: "",
-            url: ""
+            github: "https://github.com/joshherrera09/fitnesstracker",
+            deployed: "https://joshs-fitness-tracker.herokuapp.com/?id=5f9ddcd3e0a409001735da8d"
         }
     ],
 
     // Short snippet of how to contact me
-    contactSection: "",
+    contactSection: "Please feel free to contact me for more information",
 
     social: [
         {
             name: "LinkedIn",
-            url: "",
+            url: "https://www.linkedin.com/in/josh-herrera-3047b254/",
         },
         {
             name: "GitHub",
-            url: "",
+            url: "https://github.com/joshherrera09",
         },
         {
             name: "Twitter",
-            url: "",
+            url: "https://twitter.com/therealjah9",
         },
         {
             name: "Resume",
